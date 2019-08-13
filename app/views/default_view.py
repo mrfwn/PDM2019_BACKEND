48af52847b60d4a3181e3ef4dae5714990f74228
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2019-08-13 02:43:22 -0300

    Modificações para o a imagem ser enviada no corpo do email

--- app/views/default_view.py
+++ app/views/default_view.py
@@ -9,18 +9,20 @@
 ########################################################
 '''
 from app import app
-from flask import Flask,render_template,url_for, redirect
+from flask import Flask, render_template, url_for, redirect
 from flask_wtf import FlaskForm
 from flask_wtf import Form
 from werkzeug.utils import secure_filename
-from flask_wtf.file import FileField 
-from wtforms import StringField,PasswordField
+from flask_wtf.file import FileField
+from wtforms import StringField, PasswordField
 from app.services.contact_service import Contact
+
 
 class UploadForm(FlaskForm):
     email = StringField('Email')
     password = PasswordField('Senha')
     file = FileField()
+
 
 @app.route("/index")
 @app.route("/")
@@ -29,7 +31,8 @@ def home():
     contacts = Contact()
     presence = contacts.countPresence()
     agencyCount = contacts.countAgence()
-    return render_template('home_template.html',presence=presence,agencyCount=agencyCount,agencys=contacts.agencys)
+    return render_template('home_template.html', presence=presence, agencyCount=agencyCount, agencys=contacts.agencys)
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -42,6 +45,7 @@ def upload():
         return redirect(url_for('upload'))
     return render_template('upload_template.html', form=form)
 
+
 @app.route('/invit', methods=['GET', 'POST'])
 def invit():
     form = UploadForm()
@@ -50,6 +54,7 @@ def invit():
         contacts.generateInvit()
         return redirect(url_for('invit'))
     return render_template('generate_invitation.html', form=form)
+
 
 @app.route('/clear', methods=['GET', 'POST'])
 def clear():
@@ -60,13 +65,23 @@ def clear():
         return redirect(url_for('clear'))
     return render_template('clearlist_template.html', form=form)
 
+
+@app.route('/preemail', methods=['GET', 'POST'])
+def preemail():
+    form = UploadForm()
+    if form.validate_on_submit():
+        email = Contact()
+        email.sendEmailWithoutQR(form.email.data, form.password.data)
+        print("Entrou aq")
+        return redirect(url_for('preemail'))
+    return render_template('sendPreEmail_template.html', form=form)
+
+
 @app.route('/email', methods=['GET', 'POST'])
 def email():
     form = UploadForm()
     if form.validate_on_submit():
         email = Contact()
-        email.sendEmail(form.email.data,form.password.data)
+        email.sendEmail(form.email.data, form.password.data)
         return redirect(url_for('email'))
     return render_template('sendEmail_template.html', form=form)
-
-
