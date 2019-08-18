ddb085947ba276796a20f7bb6ec46ef12db1820c
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2019-08-18 18:35:43 -0300

    adicionado gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# PDM2019_BACKEND
-Solução para Prêmio de Midia 
--- config.py
+++
@@ -1,7 +0,0 @@
-DEBUG = True
-
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = f'mysql://'
-
-SECRET_KEY = "chave_secreta"
